4cb36cda104ce03df89924985a3905580d81fa4f
Benjamin BD. DAGUE <benjamin.dague@etskirsch.fr>
2020-09-14 15:24:40 +0200

    Ajout du projet AddressBook

---
+++ AddressBook/AddressBook.Client/Models/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook.Client.Models
+{
+    /// <summary>
+    ///     Représente une personne du carnet d'adresse.
+    /// </summary>
+    public class Person
+    {
+        #region Fields
+
+        /// <summary>
+        ///     Prénom de la personne.
+        /// </summary>
+        private string _FirstName;
+
+        /// <summary>
+        ///     Nom de la personne.
+        /// </summary>
+        private string _LastName;
+
+        #endregion
+
+        #region Properties
+        
+        /// <summary>
+        ///     Obtient ou définit le prénom de la personne.
+        /// </summary>
+        public string FirstName
+        {
+            get { return this._FirstName; }
+            set { this._FirstName = value; }
+        }
+
+        /// <summary>
+        ///     Obtient ou définit le nom de la personne.
+        /// </summary>
+        public string LastName
+        {
+            get => this._LastName;
+            set => this._LastName = value;
+        }
+
+        public bool IsEmpty => string.IsNullOrWhiteSpace(this.FirstName)
+                                ||
+                                string.IsNullOrWhiteSpace(this.LastName);
+
+        #endregion
+
+        #region Methods
+
+        public void ToUpper()
+        {
+            if (this.FirstName != null)
+            {
+                this.FirstName = this.FirstName.ToUpper();
+            }
+            //L'opérateur ?. permet de faire un test de null avant l'accès.
+            //En cas de valeur null, retour null.
+            //L'opérateur ?? permet d'affecter une valeur de remplacement si la partie 
+            //de gauche est null.
+            this.LastName = (this.LastName?.ToUpper()) ?? "DEFAULT";
+        }
+
+        //public bool IsEmpty() => string.IsNullOrWhiteSpace(this.FirstName) 
+        //                            ||
+        //                        string.IsNullOrWhiteSpace(this.LastName);
+
+        #endregion
+    }
+}
---
+++ AddressBook/AddressBook.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using AddressBook.Client.Models;
+using System;
+
+namespace AddressBook.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person p = new Person
+            { 
+                FirstName = "Benjamin"
+            };
+
+            p.ToUpper();
+
+            Console.WriteLine(p.FirstName + " " + p.LastName);
+
+            Console.WriteLine("Appuyez sur une touche pour terminer...");
+            Console.ReadKey();
+        }
+    }
+}
