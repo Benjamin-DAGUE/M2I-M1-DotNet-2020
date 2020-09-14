950a087f106465b8e963fe8aefc761be5f7c2a70
Benjamin BD. DAGUE <benjamin.dague@etskirsch.fr>
2020-09-14 13:59:02 +0200

    Ajout initial

---
+++ TypeRefTypeValeur/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypeRefTypeValeur
+{
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person()
+            {
+                Name = "Benjamin DAGUÉ"
+            };
+
+            ToUpper(person1);
+        }
+
+        private static void ToUpper(Person pers)
+        {
+            pers.Name = pers.Name.ToUpper();
+        }
+    }
+}
