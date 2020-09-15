0d1ef594f66c650e28a74530cf9ae8e1ffce4ef7
Benjamin BD. DAGUE <benjamin.dague@etskirsch.fr>
2020-09-15 15:04:25 +0200

    Correction exo linq

---
+++ LinqTest/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTest
+{
+    class Person : Object
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString() => this.LastName + " " + this.FirstName;
+    }
+}
