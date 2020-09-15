a9860ba90b0abeacbae660bafadc9cf857e18012
Benjamin BD. DAGUE <benjamin.dague@etskirsch.fr>
2020-09-15 12:06:24 +0200

    Ajout du cours linq et delegate

---
+++ CoursLinq/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoursLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = new List<int>();
+            Random r = new Random();
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    values.Add(r.Next(0, 100));
+            //}
+
+            //Il est possible d'utiliser Linq pour générer une collection de nombre
+            //Ici, Enumerable.Range génère une suite de nombres qui commence à 0 et contient 100 éléments
+            //Ensuite, on utilise Select pour changer la projection. Ici on remplace le nombre par un nombre aléatoire.
+            //ToList() Permet de générer la liste de résultats.
+            values = Enumerable.Range(0, 100).Select(num => r.Next(0, 100)).ToList();
+            
+            //Il est possible de faire un foreach avec la méthode ForEach
+            values.ForEach(num => Console.WriteLine(num));
+            //Parallel.ForEach permet de faire un ForEach en asynchrone
+            Parallel.ForEach(values, num => Thread.Sleep(1000));
+
+            //Where permet de filtrer une liste d'éléments.
+            //Ici, on filtre les nombres impaires pour conserver les nombres pairs.
+            //Where attend un prédicat de type Func<T, bool>
+            //On peut donc utiliser une méthode anonyme au lieu d'utiliser un méthode nommée.
+            //Where retourne un IEnumerable<int> qui représente la requête mais pas son résultat.
+            //C'est lorsque la collection sera énumérée que le filtre sera appliqué.
+            // par exemple lors d'un foreach ou lors d'un ToList().
+            IEnumerable<int> evenNumbers = values.Where((value) => value % 2 == 0);
+            //IEnumerable<int> evenNumbers = values.Where(CheckEven);
+
+            //if (true)
+            //{
+            //    //Il est possible de compléter une requête
+            //    evenNumbers = evenNumbers.Where(...).Where(...);
+            //}
+
+            evenNumbers.ToList().ForEach(num => Console.WriteLine(num));
+            //foreach (int value in evenNumbers)
+            //{
+            //    Console.WriteLine(value);
+            //}
+            //values.Where(CheckEven).Count();
+
+        }
+
+        private static bool CheckEven(int value) => value % 2 == 0;
+    }
+}
---
+++ DelegateMethods/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegateMethods
+{
+    //On peut également créer un délégué sans Action et Func
+    //Action et Func sont une simplification de cette syntaxe
+    delegate void MyDelegate(bool param);
+
+    class Worker
+    {
+        private Action<bool> _Callback;
+        public void DoWork(Action<bool> callback)
+        {
+            _Callback = callback;
+            Task.Factory.StartNew(DoWorkInternal);
+            //Il est possible de créer des méthodes anonymes.
+            //Une méthode anonyme peut être créée inline avec la syntaxe suivante
+            // (paramètre) => { corps }
+            //Task.Factory.StartNew(() =>
+            //{
+            //    Thread.Sleep(2000);
+            //    callback(true);
+            //});
+        }
+
+        private void DoWorkInternal()
+        {
+            Thread.Sleep(2000);
+            _Callback(true);
+        }
+    }
+
+    class Worker2
+    {
+        public event EventHandler WorkEnded;
+
+        public void DoWork()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                this.WorkEnded?.Invoke(this, new EventArgs());
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            Console.WriteLine("Starting work");
+            //DoWork attend un paramètre de type Action<bool>.
+            //Action et Func sont des Delegate, c'est un type qui permet de passer une référence vers une méthode.
+            //Action retourne void et Func retourne un type.
+            //Action<bool> attend donc une référence d'une méthode qui prend un booléen en paramètre et retourne void.
+            worker.DoWork(WorkEnded);
+            Console.WriteLine("Work in progress...");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Starting work2");
+
+            Worker2 worker2 = new Worker2();
+            //worker2.WorkEnded += Worker2_WorkEnded;
+            worker2.WorkEnded += (sender, arg) => Console.WriteLine("Work2 ended..." + sender.ToString());
+            worker2.DoWork();
+            Console.WriteLine("Work2 in progress...");
+
+            Console.ReadLine();
+
+        }
+
+        private static void Worker2_WorkEnded(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work2 ended...");
+        }
+
+        private static void WorkEnded(bool allWasOK)
+        {
+            Console.WriteLine("Work ended");
+        }
+
+    }
+}
